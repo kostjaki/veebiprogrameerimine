3681a446e87b1fb7a9ebe98992d3a5ff8d7fdbe3
Konstantin Blinkov <kostjaki@user.noreply.github.com>
2017-09-14 11:33:05 +0300

    Lisatud muutujad ja lisa lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,21 @@
+<?php
+	//muutujad
+	$myName = "Konstantin";
+	$myFamilyName = "Blinkov";
+	//hindan päeva osa (Võrdamine   < > <=  >= == != )
+	$hourNow = date("H");
+	$partofDay = "";
+	if ($hourNow < 8){
+		$partofDay = "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow <16){
+		$partofDay = "Koolipäev";
+	}
+	if ($hourNow > 16){
+		$partofDay = "vabaaeg";
+	}
+	echo $partofDay
+?>
 <!DOCTYPE html>
 <html>
 <body bgcolor="#10DFFB">
@@ -6,12 +24,15 @@
 	<title>Konstantin Blinkov on informaatika õppilane</title>
 </head>
 <body>
-	<h1>Konstantin Blinkov, TLÜ's</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>Konstantin Blinkov, TLÜ's</h1>
 	<p>Oppin informaatika erialal</p>
 	<p>Ja soovin edukalt loppetada korgkooli</p>
 	<p>See vebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<?php
-		echo "Algus PHP õppimine.";
+		echo "<p>Algus PHP õppimine.<?p>";
+		echo "<p> Täna on ";
+		echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo ", hetkel on " .$partofDay .".</p>";
 	?>
 </body>
 </html>
