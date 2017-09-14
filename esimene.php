69862827f77e262df2c3f18fe0a987d8b9234009
Konstantin Blinkov <kostjaki@user.noreply.github.com>
2017-09-14 10:46:53 +0300

    Lisatud on esimene PHP koodi rida

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="#10DFFB">
+<head>
+	<meta charset="utf-8">
+	<title>Konstantin Blinkov on informaatika õppilane</title>
+</head>
+<body>
+	<h1>Konstantin Blinkov, TLÜ's</h1>
+	<p>Oppin informaatika erialal</p>
+	<p>Ja soovin edukalt loppetada korgkooli</p>
+	<p>See vebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+		echo "Algus PHP õppimine.";
+	?>
+</body>
+</html>
